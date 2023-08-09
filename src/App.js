b3ef5b87b3065997a603fff509e50d8f24d612d1
sadabaevb <beka.sadabaev@gmail.com>
2023-08-09 12:23:41 +0600

    work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
+import Header from "components/Header/Header";
+import { useState } from "react";
 
 function App() {
+  const [baseCurrency, setBaseCurrency] = useState("KGS");
+
+  const handleBaseCurrencyChange = (event) => {
+    setBaseCurrency(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header
+        baseCurrency={baseCurrency}
+        onBaseCurrencyChange={handleBaseCurrencyChange}
+      />
+    </ThemeProvider>
   );
 }
 
