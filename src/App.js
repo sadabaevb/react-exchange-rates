46c687ea98dfd8fd5578bd815955f7158f36be7a
sadabaevb <beka.sadabaev@gmail.com>
2023-08-10 12:03:10 +0600

    exchanges list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  CssBaseline,
+  Grid,
+  Paper,
+  ThemeProvider,
+  Toolbar,
+} from "@mui/material";
 import theme from "./theme";
 import Header from "components/Header/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ExchangesList from "components/ExchangesList/ExchangesList";
+import axios from "axios";
+
+const CURRENCIES = ["EUR", "USD", "KGS", "KZT", "RUB"];
+
+const API_WITH_BASE_CURRENCY = (baseCurrency) =>
+  `https://v6.exchangerate-api.com/v6/8134aa0b53b806ecc886e268/latest/${baseCurrency}`;
 
 function App() {
   const [baseCurrency, setBaseCurrency] = useState("KGS");
+  const [allCurrencies, setAllCurrencies] = useState({});
 
   const handleBaseCurrencyChange = (event) => {
     setBaseCurrency(event.target.value);
   };
+
+  const fetchRates = async (base) => {
+    const response = await axios.get(API_WITH_BASE_CURRENCY(base));
+
+    const onlyNeedCurrencies = Object.entries(
+      response.data.conversion_rates
+    ).reduce((acc, [key, value]) => {
+      // console.log("acc: ", acc);
+      // console.log("key: ", key);
+      // console.log("value: ", value);
+      // console.log("- - - - - - - - - - - - - - - - - - - - - - -");
+
+      if (CURRENCIES.includes(key) && base !== key) {
+        return {
+          ...acc,
+          [key]: value,
+        };
+      } else return acc;
+    }, {});
+
+    setAllCurrencies(onlyNeedCurrencies);
+  };
+
+  useEffect(() => {
+    fetchRates(baseCurrency);
+  }, [baseCurrency]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -17,6 +57,19 @@ function App() {
         baseCurrency={baseCurrency}
         onBaseCurrencyChange={handleBaseCurrencyChange}
       />
+      <Toolbar />
+
+      <Grid sx={{ margin: "50px" }} container justifyContent="space-around">
+        <Grid item></Grid>
+        <Grid item>
+          <Paper
+            elevation={2}
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <ExchangesList currencies={allCurrencies} />
+          </Paper>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
